17b507106bb2b6c82cc1d8178fbee2f6c13265db
Karan Kadam <karank@outlook.com>
2021-05-09 22:25:37 +0930

    enabled random sleep for cron

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ from random import randint
 random_no = randint(0,59)
 print(f"Selecting a random number between 0 and 59... Selected {random_no}.")
 print(f"Sleeping for {random_no} minutes...")
-# time.sleep(random_no)
+time.sleep(random_no)
 
 print("Starting insomniac script...")
 activation_code = ""
