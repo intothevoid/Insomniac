a6f7b37617d0314f148739621ffa9375d304d40c
Karan Kadam <karank@outlook.com>
2021-05-09 22:09:34 +0930

    added script and changes for cron runs

--- start.py
+++ start.py
@@ -1,4 +1,14 @@
+import sys
+import time
 import insomniac
+from random import randint
 
+# Sleeping for random minutes
+random_no = randint(0,59)
+print(f"Selecting a random number between 0 and 59... Selected {random_no}.")
+print(f"Sleeping for {random_no} minutes...")
+# time.sleep(random_no)
+
+print("Starting insomniac script...")
 activation_code = ""
 insomniac.run(activation_code)
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /Users/sysadmin/src/python/Insomniac
+source venv/bin/activate
+python3 start.py --config "config-custom/flow/step1-interact.json"
