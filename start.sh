517ce01deaa6ba6fe6a5e3d601be7f05e6bfec8d
Karan Kadam <karank@outlook.com>
2021-05-17 00:04:13 +0930

    tweaks

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /Users/sysadmin/src/python/Insomniac
-source venv/bin/activate
+cd /home/pi/bikeshed/Insomniac
+#source venv/bin/activate
 python3 start.py --config "config-custom/flow/step1-interact.json"
